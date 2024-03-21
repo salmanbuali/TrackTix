453688d4d4b84c5c1455e57b6682dc2d8a86d934
Salman BuAli <salmanbuali@gmail.com>
2024-03-21 14:37:51 +0300

    register and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Navbar from "./components/Navbar"
+import { Routes, Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Navbar from './components/Navbar'
 function App() {
-
   return (
     <div>
       <Navbar />
-      <h1 className=" text-3xl "> hello</h1>
+      <Routes>
+        <Route path="/Register" element={<Register />}></Route>
+      </Routes>
     </div>
   )
 }
