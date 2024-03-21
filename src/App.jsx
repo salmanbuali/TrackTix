93ef147bc8e2afaa12061874abc835ff62db7346
Salman BuAli <salmanbuali@gmail.com>
2024-03-21 15:10:38 +0300

    darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/Register" element={<Register />}></Route>
-      </Routes>
+    <div className={`${darkMode && "dark"}`}>
+      <Navbar toggleDarkMode={toggleDarkMode} />
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
     </div>
   )
 }
