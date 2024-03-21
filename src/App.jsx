76d9dca60582ee0abb4b0509178ec09b1dfa4413
9Jassim <jassim9.a@gmail.com>
2024-03-21 15:06:58 +0300

    add dark mode to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import Register from './pages/Register'
 import Navbar from './components/Navbar'
+import CreateTeam from './pages/CreateTeam'
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/Register" element={<Register />}></Route>
-      </Routes>
+      <main className="dark:bg-gray-900   bg-neutral-100 h-screen">
+        <Routes>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/createteam" element={<CreateTeam />}></Route>
+        </Routes>
+      </main>
     </div>
   )
 }
--- src/pages/CreateTeam.jsx
+++ src/pages/CreateTeam.jsx
@@ -18,44 +18,46 @@ const CreateTeam = ({ user }) => {
   }
 
   return (
-    <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
-      <div class="mb-5">
-        <label
-          for="name"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    <div>
+      <form class="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div class="mb-5">
+          <label
+            for="name"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Team Name:
+          </label>
+          <input
+            type="text"
+            id="text"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            ref={formRef.name}
+          />
+        </div>
+        <div class="mb-5">
+          <label
+            for="avatar"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Team Avatar
+          </label>
+          <input
+            type="avatar"
+            id="avatar"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            ref={formRef.avatar}
+          />
+        </div>
+        <button
+          type="submit"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Team Name:
-        </label>
-        <input
-          type="text"
-          id="text"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-          ref={formRef.name}
-        />
-      </div>
-      <div class="mb-5">
-        <label
-          for="avatar"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >
-          Team Avatar
-        </label>
-        <input
-          type="avatar"
-          id="avatar"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          required
-          ref={formRef.avatar}
-        />
-      </div>
-      <button
-        type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Create
-      </button>
-    </form>
+          Create
+        </button>
+      </form>
+    </div>
   )
 }
 
