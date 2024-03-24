d2c5c77ee0092b54f8116974106a08ade5f7e097
9Jassim <jassim9.a@gmail.com>
2024-03-24 18:12:40 +0300

    finish edit profile component

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -29,6 +29,81 @@ const EditProfile = ({ id }) => {
     }
     await Client.put(`/users/${id}`, user)
   }
-  return <></>
+  return (
+    <div>
+      <div>
+        <h1 className="  text-center mb-7 mt-3  text-3xl text-gray-900 dark:text-white">
+          {user.name}
+        </h1>
+        <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-5">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              ref={formRef.name}
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              ref={formRef.email}
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Phone
+            </label>
+            <input
+              type="text"
+              id="phone"
+              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+              ref={formRef.phone}
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="avatar"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Avatar
+            </label>
+            <input
+              type="text"
+              id="avatar"
+              ref={formRef.avatar}
+              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Edit
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 export default EditProfile
