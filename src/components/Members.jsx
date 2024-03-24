2e3893789bc7aeda643395fad93c2d97020e0257
Salman BuAli <salmanbuali@gmail.com>
2024-03-24 11:11:36 +0300

    add role

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,3 +1,11 @@
+import { Menu, Transition } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
+import { Fragment } from 'react'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 const Members = ({ members, teamId }) => {
   return (
     <ul
@@ -26,6 +34,39 @@ const Members = ({ members, teamId }) => {
               {member.roles.map((role) => role.team === teamId && role.name)}
             </p>
           </div>
+          <Menu as="div" className="relative flex-none">
+            <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+              <span className="sr-only">Open options</span>
+              <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      onClick={() => {
+                        addRole(ticket._id)
+                      }}
+                      className={classNames(
+                        active ? 'bg-gray-50' : '',
+                        'block px-3 py-1 text-sm leading-6 text-gray-900'
+                      )}
+                    >
+                      Add role
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </li>
       ))}
     </ul>
