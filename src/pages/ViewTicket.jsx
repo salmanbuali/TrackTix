f7b4a475bd80812c9891e4343d46335ea9701e30
9Jassim <jassim9.a@gmail.com>
2024-03-24 21:10:13 +0300

    remove comment input when complete

--- src/pages/ViewTicket.jsx
+++ src/pages/ViewTicket.jsx
@@ -203,34 +203,37 @@ const ViewTicket = ({ user }) => {
               </ul>
 
               {/* New comment form */}
-              <div className="mt-6 flex gap-x-3">
-                <form className="relative flex-auto">
-                  <div className="overflow-hidden rounded-lg pb-12 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-                    <label htmlFor="comment" className="sr-only">
-                      Add your comment
-                    </label>
-                    <textarea
-                      rows={2}
-                      name="comment"
-                      id="comment"
-                      className="block w-full resize-none border-0 bg-transparent py-1 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="Add your comment..."
-                      defaultValue={''}
-                      ref={content}
-                    />
-                  </div>
+              {ticket.status !== 'Complete' &&
+                ticket.member.map((member) => member._id).includes(user.id) && (
+                  <div className="mt-6 flex gap-x-3">
+                    <form className="relative flex-auto">
+                      <div className="overflow-hidden rounded-lg pb-12 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+                        <label htmlFor="comment" className="sr-only">
+                          Add your comment
+                        </label>
+                        <textarea
+                          rows={2}
+                          name="comment"
+                          id="comment"
+                          className="block w-full resize-none border-0 bg-transparent py-1 px-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                          placeholder="Add your comment..."
+                          defaultValue={''}
+                          ref={content}
+                        />
+                      </div>
 
-                  <div className="absolute inset-x-0 bottom-0 flex py-2 pl-3 pr-2">
-                    <button
-                      type="submit"
-                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      onClick={addComment}
-                    >
-                      Comment
-                    </button>
+                      <div className="absolute inset-x-0 bottom-0 flex py-2 pl-3 pr-2">
+                        <button
+                          type="submit"
+                          className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                          onClick={addComment}
+                        >
+                          Comment
+                        </button>
+                      </div>
+                    </form>
                   </div>
-                </form>
-              </div>
+                )}
             </div>
           </div>
         </div>
