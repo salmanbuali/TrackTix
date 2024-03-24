1a394b1bef9f2e34ce0fe4b0d123b6aec2e8eb48
Salman BuAli <salmanbuali@gmail.com>
2024-03-24 23:08:52 +0300

    notis

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,5 +1,69 @@
-const Notifications = ({ notifications }) => {
-  return <></>
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { BellAlertIcon } from '@heroicons/react/24/outline'
+
+const Notifications = ({ open, setOpen, cancelButtonRef, notifications }) => {
+  return (
+    <div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          initialFocus={cancelButtonRef}
+          onClose={setOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto mr-5">
+            <div className="flex h-1/2 w-11/12 items-end justify-end p-4 pr-10 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6">
+                  <div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-indigo-100">
+                        <BellAlertIcon className="h-6 w-6 text-indigo-600" />
+                      </div>
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Notifications
+                      </Dialog.Title>
+                      <div className="mt-5">
+                        <ul>
+                          {notifications?.map((noti, i) => (
+                            <li key={i}>{noti.content}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </div>
+  )
 }
 
 export default Notifications
