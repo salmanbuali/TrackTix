ad1e7beacd5bbd55c4ba7f2b19c569dbbd0517bf
Salman BuAli <salmanbuali@gmail.com>
2024-03-25 22:20:05 +0300

    assign member to ticket

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+        sans: ['Inter', ...defaultTheme.fontFamily.sans]
       }
     }
   },
