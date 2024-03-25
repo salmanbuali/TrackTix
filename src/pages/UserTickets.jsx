02cda193f06a194df7b63601221b242de6710d8f
9Jassim <jassim9.a@gmail.com>
2024-03-25 21:38:54 +0300

    edit my tickets page

--- src/pages/UserTickets.jsx
+++ src/pages/UserTickets.jsx
@@ -29,6 +29,7 @@ const UserTickets = ({ user }) => {
   useEffect(() => {
     const getTickets = async () => {
       const response = await Client.get(`/users/${user?.id}`)
+      console.log(response.data.tickets)
       setTickets(response.data.tickets)
     }
     getTickets()
@@ -141,65 +142,11 @@ const UserTickets = ({ user }) => {
               </p>
 
               <Link
-                to={`/tickets/${ticket._id}/team/${teamId}`}
+                to={`/tickets/${ticket._id}/team/${ticket.team._id}`}
                 className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-90shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block dark:text-gray-200 dark:bg-gray-400/10"
               >
                 View ticket<span className="sr-only">, {ticket.name}</span>
               </Link>
-              {/* <Menu as="div" className="relative flex-none">
-                <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                  <span className="sr-only">Open options</span>
-                  <EllipsisVerticalIcon
-                    className="h-5 w-5"
-                    aria-hidden="true"
-                  />
-                </Menu.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                    {(manager || user.id === ticket.createdBy._id) && (
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            onClick={() => {
-                              edit(ticket._id)
-                            }}
-                            className={classNames(
-                              active ? 'bg-gray-50' : '',
-                              'block px-3 py-1 text-sm leading-6 text-gray-900'
-                            )}
-                          >
-                            Edit
-                            <span className="sr-only">, {ticket.name}</span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                    )}
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          onClick={() => {
-                            assign(ticket._id)
-                          }}
-                          className={classNames(
-                            active ? 'bg-gray-50' : '',
-                            'block px-3 py-1 text-sm leading-6 text-gray-900'
-                          )}
-                        >
-                          Assign<span className="sr-only">, {ticket.name}</span>
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </Menu.Items>
-                </Transition>
-              </Menu> */}
             </div>
           </li>
         ))}
