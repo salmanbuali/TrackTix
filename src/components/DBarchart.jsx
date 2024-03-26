0931dfaf93e8dd3d1a75610e0225be21ec8168c6
9Jassim <jassim9.a@gmail.com>
2024-03-26 12:37:48 +0300

    barchart done

--- src/components/DBarchart.jsx
+++ src/components/DBarchart.jsx
@@ -1,29 +1,51 @@
 import { BarChart } from '@tremor/react'
 
-const DBarChart = () => {
+const DBarChart = ({ tickets }) => {
+  let processing = 0
+  let complete = 0
+  let pending = 0
+  tickets?.forEach((ticket) => {
+    switch (ticket.status) {
+      case 'Processing':
+        processing++
+        break
+      case 'Complete':
+        complete++
+        break
+      case 'Pending':
+        pending++
+        break
+      default:
+        break
+    }
+  })
+
   const chartdata = [
     {
       name: 'Tickets',
-      'Number of proccessing tickets': 4,
-      'Number of complete tickets': 2,
-      'Number of pending tickets': 3
-
+      'Number of processing tickets': processing,
+      'Number of complete tickets': complete,
+      'Number of pending tickets': pending
     }
-  ];
+  ]
 
   // Define colors for each bar
-  const barColors = ['blue', 'green', 'red'];
+  const barColors = ['blue', 'green', 'red']
 
   return (
     <BarChart
       data={chartdata}
       index="name"
-      categories={['Number of proccessing tickets','Number of complete tickets','Number of pending tickets']}
+      categories={[
+        'Number of processing tickets',
+        'Number of complete tickets',
+        'Number of pending tickets'
+      ]}
       colors={barColors} // Assign colors array
       yAxisWidth={48}
       onValueChange={(v) => console.log(v)}
     />
-  );
-};
+  )
+}
 
 export default DBarChart
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,27 @@
-import DBarChart from "../components/DBarchart"
+import { useParams } from 'react-router-dom'
+import DBarChart from '../components/DBarchart'
+import { useEffect, useState } from 'react'
+import Client from '../services/api'
 
 const Dashboard = () => {
+  const { teamId } = useParams()
+  const [team, setTeam] = useState({})
+  const [tickets, setTickets] = useState([])
+
+  useEffect(() => {
+    const getTeam = async () => {
+      const response = await Client.get(`/teams/${teamId}`)
+      const ticketResponse = await Client.get(`/tickets/team/${teamId}`)
+      setTickets(ticketResponse.data)
+      setTeam(response.data)
+    }
+    getTeam()
+  }, [])
 
   return (
-    <div>hi
-      <DBarChart />
+    <div>
+      hi
+      <DBarChart tickets={tickets} />
     </div>
   )
 }
