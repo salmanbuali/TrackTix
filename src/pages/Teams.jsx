7dcf86397d60e561ce3b30995456590aea2f78d3
Salman BuAli <salmanbuali@gmail.com>
2024-03-27 21:30:45 +0300

    css adjustments

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -59,7 +59,7 @@ const Teams = ({ user }) => {
       >
         {teams.map((team) => (
           <Link to={`/teams/${team._id}`} key={team.name}>
-            <li className="col-span-1 flex rounded-md overflow-hidden">
+            <li className="col-span-1 flex rounded-md overflow-hidden shadow-xl">
               <div className="flex items-center justify-center text-white bg-white dark:bg-gray-800 w-16 h-16">
                 <img
                   src={team.avatar}
@@ -99,7 +99,7 @@ const Teams = ({ user }) => {
           className="rounded-lg bg-white px-3 py-2 text-sm font-medium text-gray-900 dark:text-white shadow-sm dark:hover:bg-white/20 flex items-center hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white gap-1"
           onClick={() => {
             setOpen(true)
-            setCheckInv(prev => !prev)
+            setCheckInv((prev) => !prev)
           }}
         >
           <InboxArrowDownIcon className="w-6 h-6 mr-2" />
